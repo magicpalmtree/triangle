a6b80d1ed06fcdb00a19dc6f99c74dd50dd85cb4
magicpalmtree <magicpalmtree.space@gmail.com>
2023-07-26 21:44:09 -0400

    feat: add error boundary

---
+++ src/app/error.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="grid h-screen place-content-center bg-white px-4">
+      <div className="text-center">
+        <h1 className="text-9xl font-black text-gray-200">500</h1>
+
+        <p className="mb-10 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Something went wrong!</p>
+
+        <button
+          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-2xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          onClick={() => reset()}
+        >
+          Try again
+        </button>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{ts,tsx}',
-    './src/components/**/*.{ts,tsx}',
     './src/app/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
   ],
   theme: {},
   plugins: [],
